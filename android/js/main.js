48505886d420210a65a7efc6aa7e9cf6ae9952c9
HolaMan <reality.chang@gmail.com>
2012-11-11 01:58:34 +0800

    complete the web tutorial content for app class

--- android/js/main.js
+++ android/js/main.js
@@ -2,21 +2,30 @@ $(function() {
 	var concepts_topics = [{
 		short_title: "Activity",
 		title: "基本流程元件",
-		link: 'splashscreen.html'
+		link: 'activity.html'
 	}, {
 		short_title: "Intent",
 		title: "連結Activity的Intent",
-		link: 'splashscreen.html'
+		link: 'intent.html'
 	}	
 	];
-	var blocks_topics = [{
-		short_title: "Share",
-		title: "分享到其它App",
-		link: 'shareintent.html'
+	var blocks_topics = [
+	{
+		short_title: "Toast",
+		title: "提示訊息",
+		link: 'toast.html'
 	}, {
-		short_title: "ScreenCapture",
-		title: "手機截圖",
-		link: 'screencapture.html'
+		short_title: "Dialog",
+		title: "對話視窗",
+		link: 'dialog.html'
+	}, {
+		short_title: "View & Activity",
+		title: "View與Activity的關係",
+		link: 'view_and_activity.html'
+	}, {
+		short_title: "Button",
+		title: "按鈕與Click事件",
+		link: 'buttonclick.html'
 	}, {
 		short_title: "Camera",
 		title: "照相功能",
@@ -25,12 +34,24 @@ $(function() {
 		short_title: "Gallery",
 		title: "選取相片集",
 		link: 'gallery.html'
-	}
-	, {
+	}, {
+		short_title: "ShareIntent",
+		title: "分享到其它App",
+		link: 'shareintent.html'
+	}, {
+		short_title: "ScreenCapture",
+		title: "手機截圖",
+		link: 'screencapture.html'
+	}, {
 		short_title: "SplashScreen",
 		title: "製作登入畫面",
 		link: 'splashscreen.html'
-	}];
+	}, {
+		short_title: "Vibrator",
+		title: "震動",
+		link: 'vibrator.html'
+	}
+	];
 
 	$.each(concepts_topics, function(index, topic) {
 		var topic_div = $("<div class='block' onClick='window.location.href=\"{2}\"'><p class='title'>{0}<p>{1}</div>"
